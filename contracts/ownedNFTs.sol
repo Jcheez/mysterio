ac581766435c95bb5118bbe89cb61cebc243c4f0
Jcheez <jiajunlee2@gmail.com>
2022-03-19 20:54:32 +0800

    updated ownedNFTs

--- contracts/ownedNFTs.sol
+++ contracts/ownedNFTs.sol
@@ -1,18 +1,28 @@
 pragma solidity ^0.5.0;
 
 contract ownedNFTs {
-    uint256 numNFTs;
+    uint256 private numNFTs;
+    uint256 private nextAvailableSlot;
     mapping(uint256 => address) private unwantedNFTs;
-    mapping(uint256 => uint) private NFTPrices;
+    mapping(uint256 => uint256) private NFTPrices;
 
-    function add(uint256 price, address add) public {
-        // counter for next  
-        // Find the next empty slot (address should be 0)
+    function add(uint256 price, address nftAddress) public {
+        unwantedNFTs[nextAvailableSlot] = nftAddress;
+        NFTPrices[nextAvailableSlot] = price;
+        numNFTs += 1;
+        nextAvailableSlot += 1;
     }
 
-    function delete(uint256 id) public returns (uint256, address) {
-        // Change address of ID to 0
-        // returns the address and price testing
+    function remove(uint256 id) public returns (uint256, address) {
+        require(unwantedNFTs[id] != address(0), "NFT has already been removed");
+        require(NFTPrices[id] != 0, "NFT has already been removed");
+        address nftAdd = unwantedNFTs[id];
+        uint256 price = NFTPrices[id];
+
+        unwantedNFTs[id] = address(0);
+        NFTPrices[id] = 0;
+
+        return (price, nftAdd);
     }
 
 }

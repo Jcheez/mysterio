86eba8e5668ab1a4b396638e9a550606bab8a254
Kevin <kevcxy@gmail.com>
2022-03-29 22:37:13 +0800

    MysteryStaking Contract fixes

--- contracts/MysteryStaking.sol
+++ contracts/MysteryStaking.sol
@@ -1,6 +1,6 @@
-pragma solidity >= 0.5.0;
+pragma solidity >=0.5.0;
 
-import './ERC20.sol';
+import "./ERC20.sol";
 
 contract MysteryStaking {
     ERC20 mystToken;
@@ -12,7 +12,7 @@ contract MysteryStaking {
     mapping(address => uint256) stakingBalance; // How much each address staked
     mapping(address => uint256) lastRewardCalcTime; // Start of staking for the address in unix timestamp
     mapping(address => uint256) rewardsEarned; // How much they earn in rewards
-    mapping(address => bool) isStaking; 
+    mapping(address => bool) isStaking;
 
     constructor() public {
         mystToken = new ERC20();
@@ -21,48 +21,60 @@ contract MysteryStaking {
         poolAPY = 60; // 60% a year
     }
 
-    event Transfer(address to, uint256 amount); 
-    event TokenSent(address sender, uint256 amt); 
+    event Transfer(address to, uint256 amount);
+    event TokenSent(address sender, uint256 amt);
     event GetMYST(address sender, uint256 amt);
     event TokenStaked(address sender, uint256 amt);
     event RewardsClaimed(address sender, uint256 amt);
     event TokenUnstaked(address sender, uint256 amt);
 
-    function getMYST() public payable returns(uint256) { 
+    function getMYST() public payable returns (uint256) {
         require(msg.value >= 1E16, "At least 0.01ETH needed to get MYST");
-        uint256 val = msg.value/ 1E16;
+        uint256 val = msg.value / 1E16;
         mystToken.mint(msg.sender, val);
         emit GetMYST(msg.sender, val);
     }
 
-    function transferMYST(address to, uint256 amount) private { 
+    function transferMYST(address to, uint256 amount) private {
         mystToken.transfer(to, amount);
         emit Transfer(to, amount);
     }
 
-    function stakeTokens(uint256 amt) public { //send MYST to the pool
-        if(isStaking[msg.sender]) {
-            calculateRewards(msg.sender);
+    function stakeTokens(uint256 amt) public {
+        //send MYST to the pool
+        if (isStaking[msg.sender]) {
+            rewardsEarned[msg.sender] += calculateRewards(msg.sender);
+            lastRewardCalcTime[msg.sender] = block.timestamp / 1000;
+            mystToken.transferFrom(msg.sender, address(this), amt);
+            stakingBalance[msg.sender] += amt;
+            isStaking[msg.sender] = true;
+            totalPool += amt;
+            emit TokenStaked(msg.sender, amt);
+        } else {
+            mystToken.transferFrom(msg.sender, address(this), amt);
+            stakers.push(msg.sender);
+            rewardsEarned[msg.sender] = 0;
+            lastRewardCalcTime[msg.sender] = block.timestamp / 1000;
+            stakingBalance[msg.sender] = amt;
+            isStaking[msg.sender] = true;
+            totalPool += amt;
+            emit TokenStaked(msg.sender, amt);
         }
-        mystToken.transferFrom(msg.sender, address(this), amt);
-        stakingBalance[msg.sender] += amt;
-        lastRewardCalcTime[msg.sender] = block.timestamp / 1000;
-        isStaking[msg.sender] = true;
-        totalPool += amt;
-        emit TokenStaked(msg.sender, amt);
     }
 
     function unstakeTokens(uint256 amt) public {
         require(isStaking[msg.sender], "You do not have any tokens staked!");
-        require(stakingBalance[msg.sender] >= amt, "Amount to unstake exceeds staking balance!");
+        require(
+            stakingBalance[msg.sender] >= amt,
+            "Amount to unstake exceeds staking balance!"
+        );
         unstakeClaimRewards(msg.sender);
         transferMYST(msg.sender, amt);
         totalPool -= amt;
         if (stakingBalance[msg.sender] == amt) {
             isStaking[msg.sender] = false;
             stakingBalance[msg.sender] = 0;
-        }
-        else {
+        } else {
             stakingBalance[msg.sender] -= amt;
         }
         emit TokenUnstaked(msg.sender, amt);
@@ -71,37 +83,40 @@ contract MysteryStaking {
     function claimRewards() public {
         require(isStaking[msg.sender], "You do not have any tokens staked!");
         require(rewardsEarned[msg.sender] > 0, "You do not have any rewards!");
-        calculateRewards(msg.sender);
-        uint256 memory amt = rewardsEarned[msg.sender];
+        rewardsEarned[msg.sender] += calculateRewards(msg.sender);
+        lastRewardCalcTime[msg.sender] = block.timestamp / 1000;
+        uint256 amt = rewardsEarned[msg.sender];
         transferMYST(msg.sender, amt);
         rewardsEarned[msg.sender] = 0;
         emit RewardsClaimed(msg.sender, amt);
     }
 
     function unstakeClaimRewards(address sender) private {
-        require(rewardsEarned[msg.sender] > 0, "You do not have any rewards!");
-        calculateRewards(msg.sender);
-        uint256 memory amt = rewardsEarned[msg.sender];
-        transferMYST(msg.sender, amt);
-        rewardsEarned[msg.sender] = 0;
+        require(rewardsEarned[sender] > 0, "You do not have any rewards!");
+        rewardsEarned[sender] += calculateRewards(sender);
+        lastRewardCalcTime[sender] = block.timestamp / 1000;
+        transferMYST(sender, rewardsEarned[sender]);
+        rewardsEarned[sender] = 0;
     }
 
-    function calculateRewards(address sender) private {
-        rewardsEarned[sender] += (now/1000 - lastRewardCalcTime[msg.sender]) * stakingBalance[msg.sender] * (poolAPY / 100 / 365 / 24 / 60);
-        lastRewardCalcTime[sender] = now/1000;
+    function calculateRewards(address sender) internal view returns (uint256) {
+        return
+            (block.timestamp / 1000 - lastRewardCalcTime[sender]) *
+            stakingBalance[sender] *
+            (poolAPY / 100 / 365 / 24 / 60);
         //checks for NFT and add more APY here.
     }
 
-//getter functions for testing
-    function getTotalPool() public view returns(uint256) {
+    //getter functions for testing
+    function getTotalPool() public view returns (uint256) {
         return totalPool;
     }
 
-    function getTokenBalance(address user) public view returns(uint256) {
+    function getTokenBalance(address user) public view returns (uint256) {
         return mystToken.balanceOf(user);
     }
 
-    function getStakedBalance(address user) public view returns(uint256) {
+    function getStakedBalance(address user) public view returns (uint256) {
         return stakingBalance[user];
     }
 
@@ -109,5 +124,4 @@ contract MysteryStaking {
         calculateRewards(user);
         return rewardsEarned[user];
     }
-
 }

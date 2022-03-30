a12007cb68cff23f5872eb2453f351dda3bb2ba3
Bryan <bryan00iwe@gmail.com>
2022-03-30 19:58:19 +0800

    metadata for mystnfts, updated test cases

--- test/MysteryNFT.test.js
+++ test/MysteryNFT.test.js
@@ -7,6 +7,7 @@ const MysteryNFT = artifacts.require("./MysteryNFT.sol");
 
 contract("MysteryNFT", (accounts) => {
   let contract;
+
   before(async () => {
     contract = await MysteryNFT.deployed();
   });
@@ -31,39 +32,50 @@ contract("MysteryNFT", (accounts) => {
 
   describe("minting", async () => {
     it("create new token", async () => {
-      const result = await contract.mint(123);
+      const result = await contract.mint(0, []);
       //number of tokens that exist
       const totalSupply = await contract.totalSupply();
       assert.equal(totalSupply, 1);
 
       const event = result.logs[0].args;
-      assert.equal(event.tokenId.toNumber(), 123, "id correct");
+      assert.equal(event.tokenId.toNumber(), 0, "id correct");
       assert.equal(event.from, 0x0, "from correct");
       assert.equal(event.to, accounts[0], "to correct");
     });
   });
 
   describe("indexing", async () => {
-    it("list tokens", async () => {
-    //   await contract.mint(123);
-      await contract.mint(234);
-      await contract.mint(345);
-      await contract.mint(456);
+    it("rarity check", async () => {
+      await contract.mint(1, []);
+      await contract.mint(1, []);
+      await contract.mint(2, ['0x111122223333444455556666777788889999aAaa']);
       const totalSupply = await contract.totalSupply();
 
       let result = [];
 
       for (var i = 0; i < totalSupply; i++) {
-        const id = await contract.tokenId(i);
+        const id = await contract.getRarity(i);
         result.push(id);
       }
-      
-      let expected = [123, 234, 345, 456];
+
+      let expected = [0, 1, 1, 2];
+      assert.equal(result.join(","), expected.join(","));
+    });
+
+    it("contents check", async () => {
+      const totalSupply = await contract.totalSupply();
+
+      let result = [];
+
+      for (var i = 0; i < totalSupply; i++) {
+        const id = await contract.getContents(i);
+        result.push(id);
+      }
+
+      let expected = [[],[],[],['0x111122223333444455556666777788889999aAaa']];
       assert.equal(result.join(","), expected.join(","));
     });
   });
 
-  describe("user owns", async ()=>{
-      
-  })
+  
 });
